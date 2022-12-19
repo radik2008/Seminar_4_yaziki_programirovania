d8bd69ce6cf79947c3f127f1079474dbf378ba97
Radik <Radik2008@mail.ru>
2022-12-19 16:55:35 +0500

    Решили задачу3

---
+++ Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя:");
+string username = Console.ReadLine();
+if(username == "Маша")
+{
+    Console.WriteLine("Ура, это же Маша");
+}
+else
+{
+    Console.WriteLine("Привет-");
+    Console.WriteLine(username);
+}

65a75d4795ec5a1dcb524d0712f92f32dd90c68d
Radik <Radik2008@mail.ru>
2022-12-19 21:43:16 +0500

    Решили задачу4

---
+++ Task4/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите  число от 1 до 7");
+int den = int.Parse(Console.ReadLine());
+if(den == 1)
+{
+   Console.WriteLine($"{1}  это понедельник");
+}
+if(den == 2)
+{
+   Console.WriteLine($"{2}  это вторник");
+}
+if(den == 3)
+{
+   Console.WriteLine($"{3}  это среда");
+}
+if(den == 4)
+{
+   Console.WriteLine($"{4}  это четверг");
+}
+if(den == 5)
+{
+   Console.WriteLine($"{5}  это пятница");
+}
+if(den == 6)
+{
+   Console.WriteLine($"{6}  это суббота");
+}
+if(den == 7)
+{
+   Console.WriteLine($"{7}  это воскресенье");
+}

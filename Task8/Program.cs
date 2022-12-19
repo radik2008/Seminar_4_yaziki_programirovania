4ac226202be44060e6ad65f8d764ecce4adba5b2
Radik <Radik2008@mail.ru>
2022-12-20 00:25:10 +0500

    Решили задачу ДЗ №1 с двумя числами

---
+++ Task8/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine());
+if(a > b)
+{
+    Console.WriteLine($"{a} больше {b}");
+}
+if(a < b)
+{
+    Console.WriteLine($"{a} меньше {b}");
+}
+if(a == b)
+{
+    Console.WriteLine($"{a} равно {b}");
+}

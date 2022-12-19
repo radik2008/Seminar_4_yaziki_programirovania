28a63e787700e8c3cb5d37306d985fcb00fe1ee0
Radik <Radik2008@mail.ru>
2022-12-20 01:14:34 +0500

    Решили задачу ДЗ №4 с четными  числами

---
+++ Task11/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console;
+Clear();
+WriteLine("Введите  число:");
+int a = int.Parse(Console.ReadLine());
+int count = 2;
+while(count <= a)
+{
+    WriteLine(count);
+    count+=2;
+}

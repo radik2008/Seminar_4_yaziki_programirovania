556344ea0f4b3cf5b0b5ac96bcf947ca1ef64a6b
Radik <Radik2008@mail.ru>
2022-12-20 00:55:42 +0500

    Решили задачу ДЗ №3 с четными и нечетными числами

---
+++ Task10/Program.cs
@@ -0,0 +1,6 @@
+﻿using static System.Console;
+Clear();
+WriteLine("Введите  число :");
+int a = int.Parse(Console.ReadLine());
+if(a%2 == 0) WriteLine($"{a} четное число!");
+else WriteLine($"{a} нечетное число!");

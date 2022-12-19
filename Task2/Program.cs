d2afeebbaf8c19891a85e1b6de53e68d586f554d
Radik <Radik2008@mail.ru>
2022-12-19 17:18:24 +0500

    Решили задачу2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine());
+if(a*a == b)
+{
+   Console.WriteLine($"{b} квадрат числа {a}");
+}
+else
+{
+    Console.WriteLine($"{b} не является квадратом числа {a}");
+}

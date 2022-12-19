014ccc07b40cde064bfdf98a7a8474a34d24714d
Radik <Radik2008@mail.ru>
2022-12-19 23:50:26 +0500

    Решили задачу с остатком от деления

---
+++ Task7/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+WriteLine("Введите  число трехзначное: ");
+int n = int.Parse(Console.ReadLine());
+if(n > 99 && n < 1000)
+{
+    WriteLine(n%10);
+}
+else
+{
+    WriteLine("Ввели неверное число");
+}

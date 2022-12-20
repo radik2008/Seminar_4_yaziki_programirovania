fa6b445e80501c8c4f38cede1b6a3d2f38ad757b
Radik <Radik2008@mail.ru>
2022-12-20 23:01:27 +0500

    Решили задачу лекции  поиск элемента массива

---
+++ Task14/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+Clear();
+
+int[] Array = { 23, 243, 23, 432, 42, 46, 867, 867, 876 };
+
+int count = 0;
+int faind = 42;
+while (count < Array.Length)
+{
+    if (Array[count] == faind) Write($"{count} номер элемента");
+    count++;
+}
+

f5465cdd9a898342e8e48b30b718a9f54d6fe319
Radik <Radik2008@mail.ru>
2022-12-26 18:13:54 +0500

    Решили задачу ДЗ номер2, задач3

---
+++ HomeWork1/Zadacha3/Program.cs
@@ -0,0 +1,6 @@
+﻿using static System.Console;
+
+Write("Введите номер дне недели: ");
+Write("Введите номер дня недели: ");
+int a = int.Parse(ReadLine()!);
+WriteLine(a>0&&a<8?a>5?"Да":"Нет":"Число должно быть целым от 1 до 7");

372082da2f3b5b8637b36e6800f35b8401c14628
Radik <Radik2008@mail.ru>
2022-12-26 17:30:55 +0500

    Решили задачу семинар, дополнили2

--- Task16_Seminar2/Program.cs
+++ Task16_Seminar2/Program.cs
@@ -12,5 +12,7 @@ else
 {
    WriteLine(a2); 
 }
-//int max=a1>a2?a1:a2;    можно записать так  WraitLine(a1>a2?a1:a2);
+//int max=a1>a2?a1:a2;    можно записать так  
+//WraitLine(a1>a2?a1:a2);
+//WraitLine(number/10>number%10?number/10:number%10);   или так, все решение в одной строчеке
 WriteLine(number%2==0?"Even":"odd");

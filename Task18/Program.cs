1afba2820687f5160cd96a82bc5d07ed6a12b862
Radik <Radik2008@mail.ru>
2022-12-26 18:25:14 +0500

    Решили задачу семинар, номер23

---
+++ Task18/Program.cs
@@ -0,0 +1,7 @@
+﻿using static System.Console;
+Clear();
+WriteLine("Введите первое число");
+int a = int.Parse(ReadLine());
+WriteLine("Введите второе число");
+int b = int.Parse(ReadLine());
+WriteLine(a % b == 0 ? "b кратно a" :$"{a%b} остаток от деления");

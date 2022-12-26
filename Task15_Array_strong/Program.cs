4816241c78b6364266fb2044adaff482f4c307e2
Radik <Radik2008@mail.ru>
2022-12-26 15:22:39 +0500

    Решили задачу лекции мвссивы

---
+++ Task15_Array_strong/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+Clear();
+
+void FillArray(int[] collection)
+{
+    int index = 0;
+    while (index < collection.Length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int position=0;
+    while (position < col.Length)
+    {
+        WriteLine(col[position]);
+        position++;
+    }
+}
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);

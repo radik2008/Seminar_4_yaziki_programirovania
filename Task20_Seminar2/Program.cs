ef3f48322f8bd3fc44098690c438343d2dbf4b55
Radik <Radik2008@mail.ru>
2022-12-27 21:19:43 +0500

    введите 2 числа, и проверьте является ли одно двадратом дркгого и наоборот

---
+++ Task20_Seminar2/Program.cs
@@ -0,0 +1,17 @@
+﻿// введите 2 числа, и проверьте является ли одно двадратом дркгого и наоборот
+using static System.Console;
+Clear();
+WriteLine("Введите первое число: ");
+int a = int.Parse(ReadLine());
+WriteLine("Введите второе число: ");
+int b = int.Parse(ReadLine());
+if (a == b * b)
+{
+    WriteLine($"{a} является квалратным числом числа {b} ");
+
+}
+else
+{
+    WriteLine($"{a} не является квалратным числом числа {b} ");
+
+}

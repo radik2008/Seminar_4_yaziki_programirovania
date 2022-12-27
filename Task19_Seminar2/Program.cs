e0f9e35359b6b08429abf5d15bab972c09460311
Radik <Radik2008@mail.ru>
2022-12-27 19:56:37 +0500

    Решили задачу семинар, тетральные операторы,написать программу которая принимает число и проверяет на кратность и 7 и 23

---
+++ Task19_Seminar2/Program.cs
@@ -0,0 +1,5 @@
+﻿using static System.Console;                 //написать программу которая принимает число и проверяет на кратность и 7 и 23
+Clear();
+WriteLine("Введите число: ");
+int numb = Convert.ToInt32(ReadLine());
+WriteLine(numb % 7 == 0 && numb % 23 == 0 ? "кратно 7 и 23" : "не кратно 7 и 23");

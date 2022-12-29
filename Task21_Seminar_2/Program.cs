4a0ad7d8dc7565967ed71ab865fd6943eacc87d1
Radik <Radik2008@mail.ru>
2022-12-29 13:09:16 +0500

    два стрелка стреляют по очереди по банкам

---
+++ Task21_Seminar_2/Program.cs
@@ -0,0 +1,10 @@
+﻿// два стрелка стреляют по очереди по банкам, в конце оба одновременно сбили банку, сколько было банок
+using static System.Console;
+Clear();
+WriteLine("Введите количество банок первого стрелка:");
+int num1 = Convert.ToInt32(ReadLine());
+WriteLine("Введите количество банок второго стрелка: ");
+int num2 = Convert.ToInt32(ReadLine());
+WriteLine($"Общее количество банок {num1 + num2 -1}");
+
+

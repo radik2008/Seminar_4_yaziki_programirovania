527f24cca243f895ca23ae3f0f43b3c2ce07d59e
Radik <Radik2008@mail.ru>
2022-12-29 21:57:52 +0500

    Вывести квадратиы чисел от 1 до N

---
+++ Seminar3/Zadacha5/Program.cs
@@ -0,0 +1,11 @@
+﻿//Вывести квадратиы чисел от 1 до N
+using static System.Console;
+Clear();
+Write("ВВедите число :");
+int n = int.Parse(ReadLine());
+int count= 0;
+while(count<=n)
+{
+    WriteLine(count*count);
+    count+=1;
+}

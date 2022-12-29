abe3f4f9ccadf227eff34597a7d3d793c9f80921
Radik <Radik2008@mail.ru>
2022-12-29 19:48:51 +0500

    вводим номер дня, получаем название дня недели, использую свич кейс

---
+++ Seminar3/Zadacha1/Program.cs
@@ -0,0 +1,48 @@
+﻿// вводим номер дня, получаем название дня недели, использую свич кейс
+using static System.Console;
+Clear();
+Write("ВВедите число от 1 до 7:");
+int day = int.Parse(ReadLine());
+switch (day)
+{
+    case 1:
+        {
+            WriteLine("Понедельник");
+            break;
+        }
+    case 2:
+        {
+            WriteLine("Вторник");
+            break;
+        }
+    case 3:
+        {
+            WriteLine("среда");
+            break;
+        }
+    case 4:
+        {
+            Write("Четверг");
+            break;
+        }
+    case 5:
+        {
+            Write("Пятница");
+            break;
+        }
+    case 6:
+        {
+            Write("Суббота");
+            break;
+        }
+    case 7:
+        {
+            Write("Воскресенье");
+            break;
+        }
+        default:
+        {
+            Write("Ощибка");
+            break;
+        }
+}

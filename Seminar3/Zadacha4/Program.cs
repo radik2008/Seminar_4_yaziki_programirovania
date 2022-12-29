617b6e4796f64bd861a1a25c76f9d81b638aafca
Radik <Radik2008@mail.ru>
2022-12-29 22:07:14 +0500

    Вывести квадратиы чисел от 1 до N  2

---
+++ Seminar3/Zadacha4/Program.cs
@@ -0,0 +1,14 @@
+﻿//Программа которая по координатам точек определяет растояние между ними
+using static System.Console;
+Clear();
+Write("ВВедите координаты х1 :");
+int x1 = int.Parse(ReadLine());
+Write("ВВедите координаты y1 :");
+int y1 = int.Parse(ReadLine());
+Write("ВВедите координаты х2 :");
+int x2 = int.Parse(ReadLine());
+Write("ВВедите координаты y2 :");
+int y2 = int.Parse(ReadLine());
+double i = Math.Sqrt(Math.Pow((x2 - x1),2) + Math.Pow((y2 - y1),2));
+
+Write(i);

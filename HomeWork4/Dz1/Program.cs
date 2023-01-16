7f20e6520d9539c16ea59dae626639ed43f5a858
Radik <Radik2008@mail.ru>
2023-01-17 00:23:30 +0500

    апишите программу, реализующую метод, который формирует массив случайных целых чисел из 8 элементов и выводит их на экран.

---
+++ HomeWork4/Dz1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, реализующую метод, который формирует массив случайных целых чисел из 8 элементов и выводит их на экран.
+
+
+using static System.Console;
+Clear();
+
+Write("Введите крайний верхний диапозон значения массива:");
+int a = int.Parse(Console.ReadLine());
+Write("Введите крайний нижний диапозон значения массива:");
+int b = int.Parse(Console.ReadLine());
+Write("Введите длинну массивва:");
+int с = int.Parse(Console.ReadLine());
+
+void FillArray(int[] collection)
+{
+    for (int i = 0; i < collection.Length; i++)
+    {
+         collection[i] = new Random().Next(a);
+        //i = i + 1;
+        i++;
+    }
+        
+}
+
+void PrintArray(int[] Ar)
+{
+    for (int i = 0; i < Ar.Length; i++)
+    {
+        Write($"{Ar[i]} ");
+    }
+}
+
+int [] array = new int[с];
+FillArray(array);
+PrintArray(array);
--- Seminar_4/Zada4_sem1/Program.cs
+++ Seminar_4/Zada4_sem1/Program.cs
@@ -1,5 +1,6 @@
 ﻿// написать программу котораяя принимает число А и выдает сумму от 1 до А   
 // написать программу котораяя принимает число А и выдает произведени  от 1 до А   
+//написать программу котораяя принимает число А и выдает сумму от 1 до А на которые А делиться без остатка
 
 using static System.Console;
 Clear();
@@ -13,7 +14,7 @@ int GetSum(int A)
     {
         result += A;
         A--;  // вычитать по одному
-    }  
+    }
     return result;
 }
 
@@ -24,7 +25,23 @@ int GetProizvedenie(int A)
     {
         result *= A;
         A--;  // вычитать по одному
-    }  
+    }
+    return result;
+}
+
+int GetSumNot(int A)
+{
+    int result = 0;
+    int i = 1;
+    while (i < A)
+    {
+        if (A % i == 0)
+        {
+            result = result + i;
+        }
+        i++;
+    }
+
     return result;
 }
 
@@ -33,8 +50,10 @@ WriteLine();
 int count = int.Parse(ReadLine());
 int sum = GetSum(count);
 int proiz = GetProizvedenie(count);
+int sum2 = GetSumNot(count);
 WriteLine();
 WriteLine($"сумма чисел от 1 до А равна {sum}");
 WriteLine();
 WriteLine($"произведение чисел от 1 до А равна {proiz}");
 WriteLine();
+WriteLine($"сумма чисел от 1 до А равна которые деляться без остатка {sum2}");

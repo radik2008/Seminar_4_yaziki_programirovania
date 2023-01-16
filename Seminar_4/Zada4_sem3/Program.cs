cf3639d779faceab23c27ebce55a5fdc22e5cb2d
Radik <Radik2008@mail.ru>
2023-01-16 21:24:48 +0500

    задача, c функциями написать прогу котррая выдает количество чисел в числе

---
+++ Seminar_4/Zada4_sem3/Program.cs
@@ -0,0 +1,20 @@
+﻿// написатьт программу которая выдает количество чисел в числе, использовать методы
+
+using static System.Console;
+Clear();
+
+int CountNumbers(int A)
+{
+    int result = 0;
+    while (A > 0)
+    {
+        result = result + 1;
+        A /= 10;     //   A=A/10
+    }
+
+    return result;
+}
+WriteLine("Input number:");
+int.TryParse(ReadLine(), out int num);
+int count = CountNumbers(num);
+WriteLine($"Count numbers = {count}");

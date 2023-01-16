2f70b8efb17aab54d4e8deb8a74c1d88223e2e32
Radik <Radik2008@mail.ru>
2023-01-16 20:49:32 +0500

    задача, c функциями наити произведение и сумму от 1 до А

---
+++ Seminar_4/Zada4_sem1/Program.cs
@@ -0,0 +1,40 @@
+﻿// написать программу котораяя принимает число А и выдает сумму от 1 до А   
+// написать программу котораяя принимает число А и выдает произведени  от 1 до А   
+
+using static System.Console;
+Clear();
+WriteLine("Hello, World!");
+
+int GetSum(int A)
+
+{
+    int result = 0;
+    while (A > 0)
+    {
+        result += A;
+        A--;  // вычитать по одному
+    }  
+    return result;
+}
+
+int GetProizvedenie(int A)
+{
+    int result = 1;
+    while (A > 0)
+    {
+        result *= A;
+        A--;  // вычитать по одному
+    }  
+    return result;
+}
+
+WriteLine("Введите число: ");
+WriteLine();
+int count = int.Parse(ReadLine());
+int sum = GetSum(count);
+int proiz = GetProizvedenie(count);
+WriteLine();
+WriteLine($"сумма чисел от 1 до А равна {sum}");
+WriteLine();
+WriteLine($"произведение чисел от 1 до А равна {proiz}");
+WriteLine();

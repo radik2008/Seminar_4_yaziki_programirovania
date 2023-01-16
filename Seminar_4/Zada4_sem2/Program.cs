a32cc207ac6d0decd700365c697b43dcc9691528
Radik <Radik2008@mail.ru>
2023-01-16 21:08:46 +0500

    задача, c функциями написать прогу котррая выдает сумму чиселв числе

---
+++ Seminar_4/Zada4_sem2/Program.cs
@@ -0,0 +1,20 @@
+﻿// написатьт программу которая выдает сумму чисел в числе, использовать методы
+
+using static System.Console;
+Clear();
+
+int CountSum(int A)
+{
+    int result = 0;
+    while (A > 0)
+    {
+        result = result + A % 10;
+        A = A / 10;
+    }
+
+    return result;
+}
+WriteLine("Input number:");
+int num = int.Parse(ReadLine());
+int sum = CountSum(num);
+WriteLine($"Sum numbers = {sum}");

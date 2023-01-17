65b6001247d0f349e32d2a84e8aff68ce51615f3
Radik <Radik2008@mail.ru>
2023-01-18 00:32:28 +0500

    /задача дан масиив и диапозон, посчитать сколько чисел попадает в диапозон

---
+++ Seminar5/zd2/Program.cs
@@ -0,0 +1,60 @@
+﻿//задача написать программу которая изменяет значения элементов массива на противоположные
+using static System.Console;
+Clear();
+
+Write("Введите размер массива, мин и макс значение через пробел: ");
+string[] parameters = ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);  // вместо " " можно написать new char[]{' ', ':', '#'  } и перечислить все разделители
+
+int[] array = GetArr(int.Parse(parameters[0]), int.Parse(parameters[1]), int.Parse(parameters[2]));
+PrintArray(array);
+WriteLine();
+ChangeArr(array);
+PrintArray(array);
+
+Write("Введите искомый элемент: ");
+int.TryParse(ReadLine(), out int looc);
+LokingElement(array);
+
+void LokingElement(int[]arra, int loocing)
+{
+    foreach (int item in arra)
+    {
+        if(arra[item]=loocing)
+        {
+            WriteLine("Нужный элемент найден");
+        }
+        else
+        {
+            WriteLine("Нужный элемент не найден");
+        }
+    }
+}
+
+void ChangeArr(int[]arra)
+{
+    for (int i = 0; i < arra.Length; i++)
+    {
+        arra[i]=arra[i]*(-1);
+    }
+}
+
+int[] GetArr(int size, int min, int max)
+{
+    int[] Result = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        Result[i] = rnd.Next(min, max + 1);
+    }
+    return Result;
+}
+
+void PrintArray(int[] arr)
+{
+    Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Write($"{arr[i]}, ");
+    }
+    Write($"{arr[arr.Length - 1]}]");
+}
---
+++ Seminar5/zd3/Program.cs
@@ -0,0 +1,69 @@
+﻿//задача дан масиив и диапозон, посчитать сколько чисел попадает в диапозон
+using static System.Console;
+Clear();
+
+Write("Введите крайний верхний диапозон значения массива:");
+int a = int.Parse(Console.ReadLine());
+Write("Введите крайний нижний диапозон значения массива:");
+int b = int.Parse(Console.ReadLine());
+Write("Введите длинну массивва:");
+int с = int.Parse(Console.ReadLine());
+
+int[] array = new int[с];
+
+Write("Введите диапозон чисел  через пробел: ");
+string[] parameters = ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);  // вместо " " можно написать new char[]{' ', ':', '#'  } и перечислить все разделители
+
+
+FillArray(array);
+PrintArray(array);
+WriteLine();
+
+SearshDiapozone(array, int.Parse(parameters[0]), int.Parse(parameters[1]));
+WriteLine();
+
+WriteLine(Sears2(array, int.Parse(parameters[0]), int.Parse(parameters[1])));
+
+
+void SearshDiapozone(int[] arr, int min, int max)          ///первый вариан решения он сразу считает и выводит
+{
+    int count = 0;
+    foreach (var item in arr)
+    {
+        if (item>=min && item<=max) count = count + 1;
+    }
+    Write($"В этом диапозоне {count} чисел");
+}
+
+int Sears2(int[] arr, int min, int max)                   //второй вариант решения просто считатет
+{
+    int Result=0;
+    foreach (var item in arr)
+    {
+        if (item>=min && item<=max) Result++;
+    }
+    return Result;
+}
+
+
+void FillArray(int[] collection)
+{
+    for (int i = 0; i < collection.Length; i++)
+    {
+        collection[i] = new Random().Next(b, a);
+    }
+
+}
+
+
+
+void PrintArray(int[] Ar)
+{
+     Write("[");
+    for (int i = 0; i < Ar.Length - 1; i++)
+    {
+        Write($"{Ar[i]}, ");
+    }
+    Write($"{Ar[Ar.Length - 1]}]");
+
+}

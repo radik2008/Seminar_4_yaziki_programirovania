8b4d722378e3ff88b38d9a8a098bbddafc0c50d7
Radik <Radik2008@mail.ru>
2023-01-22 00:54:15 +0500

    програма получает 3 числа и проверяет возможен ли такой треугольник

---
+++ Seminar6/sem2/Program.cs
@@ -0,0 +1,18 @@
+﻿//задача1 програма получает 3 числа и проверяет возможен ли такой треугольник 
+using static System.Console;
+Clear();
+
+Write("Введите 2 чмсла через через пробел: ");
+string[] parameters = ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);  // вместо " " можно написать new char[]{' ', ':', '#'  } и перечислить все разделители
+
+
+
+
+WriteLine(Check(int.Parse(parameters[0]), int.Parse(parameters[1]), int.Parse(parameters[2]))?"нет":"да");
+
+bool Check(int a, int b, int c )
+{
+    if (a+b<=c|| a+c<=b || c+b<=a) return true;
+   
+    return false;
+}

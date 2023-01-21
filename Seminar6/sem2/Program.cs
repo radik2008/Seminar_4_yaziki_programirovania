8c3a8e5871b58fdcb66ee1d112901201c38fe557
Radik <Radik2008@mail.ru>
2023-01-22 01:36:59 +0500

    //задача1 програма получает 3 числа и проверяет возможен ли такой треугольник

--- Seminar6/sem2/Program.cs
+++ Seminar6/sem2/Program.cs
@@ -2,7 +2,7 @@
 using static System.Console;
 Clear();
 
-Write("Введите 2 чмсла через через пробел: ");
+Write("Введите 3 чмсла, стороны треугольника через через пробел: ");
 string[] parameters = ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);  // вместо " " можно написать new char[]{' ', ':', '#'  } и перечислить все разделители
 
 
@@ -11,8 +11,11 @@ string[] parameters = ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntrie
 WriteLine(Check(int.Parse(parameters[0]), int.Parse(parameters[1]), int.Parse(parameters[2]))?"нет":"да");
 
 bool Check(int a, int b, int c )
-{
-    if (a+b<=c|| a+c<=b || c+b<=a) return true;
+// // {
+// //     if (a+b<=c|| a+c<=b || c+b<=a) return true;
    
-    return false;
+// //     return false;
+// }
+{
+    return (a+b<=c|| a+c<=b || c+b<=a);  //сократив все что выше расписал
 }

fd3515a5e8db6050a23f52d98c2f65b1fbd6aa16
Radik <Radik2008@mail.ru>
2023-01-21 22:00:56 +0500

    //  Нахождение факториала и фиборачи через рекурсию

--- Seminar6/seminar.factorial/Program.cs
+++ Seminar6/seminar.factorial/Program.cs
@@ -1,4 +1,4 @@
-﻿// Нахождение факториала
+﻿// Нахождение факториала   и фиборачи, чрез рекурсию
 
 
 int Factorial(int n)
@@ -6,9 +6,22 @@ int Factorial(int n)
  // 1! = 1
  // 0! = 1
  if(n == 1) return 1;
- else return n * Factorial(n-1);
+ else return n= n * Factorial(n-1);
 }
-for (int i = 1; i < 40; i++)
+for (int i = 1; i < 10; i++)
 {
  Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+
+Console.WriteLine();
+
+
+int Fibonacci(int n)
+{
+ if(n == 1 || n == 2) return 1;
+ else return n = Fibonacci(n-1) + Fibonacci(n-2);
+}
+for (int i = 1; i < 10; i++)
+{
+ Console.WriteLine($"{i}fibo = {Fibonacci(i)}");
 }

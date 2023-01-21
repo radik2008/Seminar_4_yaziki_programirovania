4a52719ba2cd41bf0733c9661be3a7264a2eb397
Radik <Radik2008@mail.ru>
2023-01-22 01:26:28 +0500

    // // программа переводит десятичное в двоичное

---
+++ Seminar6/sem3/Program.cs
@@ -0,0 +1,21 @@
+﻿// программа переводит десятичное в двоичное
+using static System.Console;
+Clear();
+WriteLine("Введите число: ");
+int.TryParse(ReadLine(), out int num);
+
+WriteLine($"{decitToBinary(num)}");
+
+string decitToBinary(int iNnum)
+{
+    string result = string.Empty;
+    int rem;  //    остаток
+    while (iNnum > 0)
+    {
+        rem = iNnum % 2;
+        iNnum = iNnum / 2;
+        result = rem.ToString() + result;
+    }
+    return result;
+
+}

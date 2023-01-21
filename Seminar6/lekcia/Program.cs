404e23e70fd8c5dad9dfdd6b00981ea4f5cdbc92
Radik <Radik2008@mail.ru>
2023-01-21 20:55:57 +0500

    // создание и заполнение матрицы

---
+++ Seminar6/lekcia/Program.cs
@@ -0,0 +1,33 @@
+﻿// создание и заполнение матрицы
+using static System.Console;
+Clear();
+
+void PrintArray(int[,] matr)
+{
+ for (int i = 0; i < matr.GetLength(0); i++)
+ {
+ for (int j = 0; j < matr.GetLength(1); j++)
+ {
+ Write($"{matr[i, j]} ");
+ }
+WriteLine();
+ }
+}
+
+void FillArray(int[,] matr)
+{
+ for (int i = 0; i < matr.GetLength(0); i++)
+ {
+ for (int j = 0; j < matr.GetLength(1); j++)
+ {
+ matr[i,j] = new Random().Next(1,10);//[1; 10)
+ }
+ }
+}
+
+
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+WriteLine();
+PrintArray(matrix);

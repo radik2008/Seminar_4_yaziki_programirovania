31556dbf15d8ab82965de9cd8cb5f72c039be683
Radik <Radik2008@mail.ru>
2023-01-23 01:17:26 +0500

    // скопировать массив через метод

---
+++ Seminar6/sem4/Program.cs
@@ -0,0 +1,26 @@
+﻿// скопировать массив через метод
+using static System.Console;
+Clear();
+Write("Введите размер массива: ");
+int.TryParse(ReadLine(), out int arraySize);
+int[] Ar = GetArra(arraySize);
+
+string StringArray = String.Join(", ", Ar);  // так мы быстро выводим массив на экран
+WriteLine($"[{StringArray}]");                // так мы быстро выводим массив на экран
+
+
+
+
+WriteLine($"копия [{String.Join(", ", CopyAr(Ar))}]");
+
+int[] GetArra(int size)
+{
+    int[] Array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Array[i] = new Random().Next(1, 10);
+    }
+    return Array;
+}
+
+int[] CopyAr(int[] Ar)//                    копия массива на осонове создания нового

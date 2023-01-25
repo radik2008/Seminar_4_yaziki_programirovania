d42affea57bab68ba54af8678fa7267ca72ab2e2
Radik <Radik2008@mail.ru>
2023-01-26 00:59:49 +0500

    //матрица

---
+++ Seminar_7/lek_Matrix/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+// в случайных точках консоли вывести случайные числа
+Console.Clear();
+Random r = new Random();
+Console.CursorVisible = false;
+while (true)
+{
+    Console.SetCursorPosition(     //  метод обрашения к позиции нв экране
+        left: r.Next(Console.WindowWidth),           //  обрашение по ширине
+        top: r.Next(Console.WindowHeight)    //  обрашение по высоте
+    );
+    Console.Write(r.Next(10));
+    Thread.Sleep(100);   // задержка по времени
+}

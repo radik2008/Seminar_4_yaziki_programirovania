d9b179b5d64fbf372d5d826dcef480aa9d913e30
Radik <Radik2008@mail.ru>
2023-01-27 15:54:14 +0500

    Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти их квадраты22

--- Seminar_7/zd2/Program.cs
+++ Seminar_7/zd2/Program.cs
@@ -1,2 +1,75 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel;
+using System;
+// Тимур: **Задача 49:** Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+// Например, изначально массив выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Новый массив будет выглядеть вот так:
+// 1 4 7 2
+// 5 **81** 2 **9**
+// 8 4 2 4
+
+// Создание двухмерного масива, и вывод на экран
+using static System.Console;
+
+Clear();
+Write("Введите размер матрицы и диапазон значений через пробел: ");
+string[] parameters = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+int[,] array = GetMatrixArray(int.Parse(parameters[0]), int.Parse(parameters[1]), int.Parse(parameters[2]), int.Parse(parameters[3]));
+PrintMatrixArray(array);
+WriteLine();
+int[,] array2 = CheangeArray(array);
+
+PrintMatrixArray(array2);
+
+int[,] CheangeArray(int[,] arr)
+{
+    int[,] resultArray = new int[arr.GetLength(0), arr.GetLength(1)];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                resultArray[i, j] = arr[i, j] * arr[i, j];
+            }
+            else
+            {
+               resultArray[i, j] = arr[i, j];
+            }
+        }
+    }
+    resultArray[0, 0] = arr[0, 0];
+    return resultArray;
+
+}
+
+int[,] GetMatrixArray(int rows, int columns, int minValue, int maxValue)
+{
+    Random rnd = new Random();
+    int[,] resultArray = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            resultArray[i, j] = rnd.Next(minValue, maxValue + 1);
+        }
+    }
+    return resultArray;
+}
+
+void PrintMatrixArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j],5} ");
+        }
+        WriteLine();
+    }
+}
+

0e0f3904011f59abf298610ca29a7940738fe833
Radik <Radik2008@mail.ru>
2023-01-27 16:30:54 +0500

    Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти их квадрат три варианта решения2

--- Seminar_7/zd2/Program.cs
+++ Seminar_7/zd2/Program.cs
@@ -20,30 +20,68 @@ string[] parameters = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntri
 int[,] array = GetMatrixArray(int.Parse(parameters[0]), int.Parse(parameters[1]), int.Parse(parameters[2]), int.Parse(parameters[3]));
 PrintMatrixArray(array);
 WriteLine();
+
 int[,] array2 = CheangeArray(array);
-
 PrintMatrixArray(array2);
+WriteLine();
 
-int[,] CheangeArray(int[,] arr)
+int[,] array3 = CheangeArray2(array);
+PrintMatrixArray(array3);
+WriteLine();
+
+CheangeArray3(array);
+PrintMatrixArray(array);
+WriteLine();
+
+int[,] CheangeArray(int[,] arr)              // через новый масив, так же можно через изменение имеюшегося тогда будет метод воид
 {
     int[,] resultArray = new int[arr.GetLength(0), arr.GetLength(1)];
     for (int i = 0; i < arr.GetLength(0); i++)
     {
         for (int j = 0; j < arr.GetLength(1); j++)
         {
-            if (i % 2 == 0 && j % 2 == 0)
+            if (i % 2 == 0 && j % 2 == 0 && i != 0 && j != 0)
             {
                 resultArray[i, j] = arr[i, j] * arr[i, j];
             }
             else
             {
-               resultArray[i, j] = arr[i, j];
+                resultArray[i, j] = arr[i, j];
             }
         }
     }
-    resultArray[0, 0] = arr[0, 0];
     return resultArray;
 
+}
+
+int[,] CheangeArray2(int[,] arra)              // через новый масив, методом клонирования
+{
+    int[,] resultArray = arra.Clone() as int[,];
+    for (int i = 0; i < arra.GetLength(0); i++)
+    {
+        for (int j = 0; j < arra.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0 && i != 0 && j != 0)
+            {
+                resultArray[i, j] = arra[i, j] * arra[i, j];
+            }
+        }
+    }
+    return resultArray;
+}
+
+void CheangeArray3(int[,] arr)              // через void замена имеющегося
+{
+    for (int i = 2; i < arr.GetLength(0); i+=2)
+    {
+        for (int j = 2; j < arr.GetLength(1); j+=2)
+        {
+            if (i % 2 == 0 && j % 2 == 0 && i != 0 && j != 0)
+            {
+                arr[i, j] *= arr[i, j];
+            }
+        }
+    }
 }
 
 int[,] GetMatrixArray(int rows, int columns, int minValue, int maxValue)

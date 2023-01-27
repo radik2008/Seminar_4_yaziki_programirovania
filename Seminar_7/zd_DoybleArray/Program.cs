b9fe940f6a86615a5e0742bddf562ebced132abd
Radik <Radik2008@mail.ru>
2023-01-27 17:33:26 +0500

    // Создание двухмерного масива, и вывод на экран, и лайфхаки

--- Seminar_7/zd_DoybleArray/Program.cs
+++ Seminar_7/zd_DoybleArray/Program.cs
@@ -1,21 +1,30 @@
-﻿Clear();
-Write("Введите размер матрицы и диапазон значений через пробел: ");
-string[] parameters=ReadLine()!.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+﻿// Создание двухмерного масива, и вывод на экран, и лайфхаки
+using static System.Console;
 
-int[,] array=GetMatrixArray(int.Parse(parameters[0]),int.Parse(parameters[1]),int.Parse(parameters[2]),int.Parse(parameters[3]));
+Clear();
+Write("Введите размер матрицы и диапазон значений через пробел: ");
+
+
+// string[] parameters = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+// int[,] array = GetMatrixArray(int.Parse(parameters[0]), int.Parse(parameters[1]), int.Parse(parameters[2]), int.Parse(parameters[3]));
+
+int[] intParams=Array.ConvertAll(ReadLine()!.Split(" ",StringSplitOptions.RemoveEmptyEntries),int.Parse); // сразу перевели строчку в масив из чисел
+// вместо " " можно написать new char[]{' ', ':', '#'  }  или new string []{" ", "-", ";", ","}или и перечислить все разделители
+
+int[,] array = GetMatrixArray((intParams[0]), (intParams[1]), (intParams[2]), (intParams[3]));    //
+
 PrintMatrixArray(array);
 
 int[,] GetMatrixArray(int rows, int columns, int minValue, int maxValue)
 {
-    Random rnd=new Random();
+    Random rnd = new Random();
+    int[,] resultArray = new int[rows, columns];
 
-    int[,] resultArray=new int[rows,columns];
-
-    for (int i = 0; i < rows; i++);
+    for (int i = 0; i < rows; i++) 
     {
         for (int j = 0; j < columns; j++)
         {
-        resultArray[i,j]=rnd.Next(minValue,maxValue+1);
+            resultArray[i, j] = rnd.Next(minValue, maxValue + 1);
         }
     }
     return resultArray;
@@ -27,7 +36,7 @@ void PrintMatrixArray(int[,] inArray)
     {
         for (int j = 0; j < inArray.GetLength(1); j++)
         {
-            Write($"{inArray[i,j],5} ");
+            Write($"{inArray[i, j],5} ");
         }
         WriteLine();
     }

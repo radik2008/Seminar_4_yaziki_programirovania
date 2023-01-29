510068c12b277302f55faeab1567d9b8f1c30887
Radik <Radik2008@mail.ru>
2023-01-29 10:39:31 +0500

    // Какого цвета клетка шахматной доски, если начинается с черной

---
+++ Seminar_7/zd4Chess2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Какого цвета клетка шахматной доски, если начинается с черной
+
+
+using static System.Console;
+
+Clear();
+Write("Введите координаты клетки через пробел: ");
+
+string parametrs = ReadLine();
+int param1 = parametrs[0];    ////////////  используем номер символа, тип данных чар
+int param2 = parametrs[1];
+
+WriteLine(param1);
+WriteLine(param2);
+
+if((param1+param2)%2==0)
+    {
+        Write("Клетка черная");
+    }
+else
+    {
+        Write("Клетка белая");
+    }

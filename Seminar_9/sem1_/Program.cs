e17c70df75baee70cdd81626f1ec4cc3e1c7b373
Radik <Radik2008@mail.ru>
2023-02-01 18:24:05 +0500

    Собрать строчку с числами от а до б    первое условие a<b и второе a>b

---
+++ Seminar_9/sem1_/Program.cs
@@ -0,0 +1,47 @@
+﻿
+// Собрать строчку с числами от а до б    первое условие a<b и второе a>b
+
+using static System.Console;
+Clear();
+
+string NumbFor(int a, int b)   /// через цикл а<b
+{
+    string res = String.Empty;
+    for (int i = a; i <= b; i++)
+    {
+        res += $"{i}  ";
+    }
+    return res;
+}
+
+string NumbRec(int a, int b)          /// чере рекурсию а<b
+{
+    if (a <= b) return $"{a}  " + NumbRec(a + 1, b);
+    else return String.Empty;
+}
+
+WriteLine(NumbFor(1, 10));
+WriteLine(NumbRec(1, 10));
+
+string NumbFor2(int a, int b)   /// через цикл а<b
+{
+    string res = String.Empty;
+    for (int i = a; i >= b; i--)
+    {
+        res += $"{i}  ";
+    }
+    return res;
+}
+
+string NumbRec2(int a, int b)          /// чере рекурсию а<b
+{
+    if (a <= b) return NumbRec2(a + 1, b) + $"{a} ";
+    else return String.Empty;
+}
+
+WriteLine(NumbFor2(10, 1));
+WriteLine(NumbRec2(1, 10));
+
+
+
+

60a70a71472a7d8e3fe2aa88c59505b49fc05e7b
Radik <Radik2008@mail.ru>
2023-02-01 22:46:45 +0500

    // Вычислить А в степени n

--- Seminar_9/sem2/Program.cs
+++ Seminar_9/sem2/Program.cs
@@ -19,10 +19,17 @@ int PowerRec(int a, int n)          /// чере рекурсию
     else return PowerRec(a, n-1)*a;
 }
 
+int PowerRec2(int a, int n)          /// чере рекурсию 
+{
+    if (n == 0) return 1;
+    else if(n%2==0) return PowerRec2(a*a, n/2);
+    else return PowerRec(a, n-1)*a;
+}
+
 
 WriteLine(PowerFor(9, 5));
-WriteLine(PowerRec(5, 7));
-
+WriteLine(PowerRec(4, 6));
+WriteLine(PowerRec2(4, 6));
 
 
 

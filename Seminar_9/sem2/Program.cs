933786ffeeaac7490e564e46cdd4c73f290af25e
Radik <Radik2008@mail.ru>
2023-02-01 22:38:08 +0500

    // Вычислить А в степени n

---
+++ Seminar_9/sem2/Program.cs
@@ -0,0 +1,28 @@
+﻿
+// Вычислить А в степени n
+using static System.Console;
+Clear();
+
+int PowerFor(int a, int n)   /// через цикл
+{
+    int res = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        res *= a;
+    }
+    return res;
+}
+
+int PowerRec(int a, int n)          /// чере рекурсию 
+{
+    if (n == 0) return 1;
+    else return PowerRec(a, n-1)*a;
+}
+
+
+WriteLine(PowerFor(9, 5));
+WriteLine(PowerRec(5, 7));
+
+
+
+

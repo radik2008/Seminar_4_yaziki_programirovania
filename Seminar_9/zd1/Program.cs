bce5cc9a423652ceee73b37f81b858a387fdee9e
Radik <Radik2008@mail.ru>
2023-02-02 03:48:39 +0500

    Задача 63:** Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.

---
+++ Seminar_9/zd1/Program.cs
@@ -0,0 +1,33 @@
+﻿// **Задача 63:** Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+
+
+using static System.Console;
+Clear();
+
+string result= GetNumbers1(20);
+WriteLine(result);
+
+WriteLine(GetNumbers1(20));
+
+PrintNumbers(20);
+
+string GetNumbers(int num)
+{
+    if (num == 1) return num.ToString();
+    else return $"{GetNumbers(num - 1)},{num}";
+}
+
+string GetNumbers1(int num)
+{
+    return (num == 1) ? num.ToString() : $"{GetNumbers1(num - 1)},{num}";
+}
+
+void PrintNumbers(int num)
+{
+    if (num == 1) Write(num);
+    else
+    {
+        PrintNumbers(num - 1);
+        Write($",{num}");
+    }
+}

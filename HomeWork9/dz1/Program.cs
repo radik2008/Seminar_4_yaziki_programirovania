b5e9c4a3b12d9c3cca1cb0d42abfda3736a60581
Radik <Radik2008@mail.ru>
2023-02-02 06:22:36 +0500

    Напишите программу, которая реализует рекурсивный метод нахождения суммы натуральных элементов в промежутке от M до N.

---
+++ HomeWork9/dz1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 66: Напишите программу, которая реализует рекурсивный метод нахождения суммы натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+using static System.Console;
+Clear();
+
+
+WriteLine("Введите число первое число:");
+int.TryParse(ReadLine(), out int num1);
+
+WriteLine("Введите число второе число:");
+int.TryParse(ReadLine(), out int num2);
+
+WriteLine($"сумма  чисел от {num1} до {num2} равна {SumNum1(num1, num2)}");
+
+WriteLine($"сумма  чисел от {num1} до {num2} равна {SumNum2(num1, num2)}");
+
+int SumNum1(int a1, int b1)
+{
+    if (a1 == b1) return a1;
+    else return SumNum1(a1, b1-1) + b1;
+}
+
+int SumNum2(int a2, int b2)
+{
+    if (a2 == b2) return b2;
+    else return SumNum2(a2 + 1 , b2) + a2;
+}
+
+
+
---
+++ Seminar_9/zd2/Program.cs
@@ -0,0 +1,23 @@
+﻿// **Задача 67:** Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+// 453 -> 12
+
+// 45 -> 9
+
+
+using static System.Console;
+Clear();
+
+
+WriteLine("Введите число :");
+int.TryParse(ReadLine(), out int num);
+WriteLine($"сумма  цифр {num} равна {SumNum(num)}");
+
+int SumNum(int n)
+{
+    if (n == 0) return 0;
+    else return SumNum(n/10) + n%10;
+}
+
+
+
